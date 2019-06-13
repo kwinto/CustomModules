ea8a2fa4b9a92f447dbfb2d9f2986ee1dccfab34
Alexander Teusz <a.teusz@cognigy.com>
2019-06-13 14:13:19 +0200

    beta node

--- modules/kofax/src/module.ts
+++ modules/kofax/src/module.ts
@@ -4,32 +4,39 @@ import axios from 'axios';
  * Runs a robot
  * @arg {SecretSelect} `secret` The configured secret to use
  * @arg {JSON} `body` The data body for POST request
- * @arg {CognigyScript} `store` Where to store the result
+ * @arg {CognigyScript} `contextStore` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function RunRobot(input: IFlowInput, args: { secret: CognigySecret, robot: string, project: string, body: JSON, store: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
+async function RunRobot(input: IFlowInput, args: { secret: CognigySecret, robot: string, project: string, body: JSON, contextStore: string, stopOnError: boolean }): Promise<IFlowInput | {}> {
 
+    const { secret, body, contextStore, stopOnError } = args;
+    const { api_key } = secret;
     // Check if secret exists and contains correct parameters
-    if (!args.secret || !args.secret.api_key) return Promise.reject("Secret not defined or invalid.");
-    if (!args.body) return Promise.reject("No JSON body defined.");
+    if (!secret) throw new Error('Not secret defined.');
+    if (!body) throw new Error('No JSON body defined.');
 
-    return new Promise((resolve, reject) => {
+    // Check if the secret is given
+    if (!api_key) throw new Error("The secret is missing the 'api_key' field.");
 
-        axios.post(`https://request-forwarder.cognigy.ai/forward`, args.body, {
+    try {
+        const response = await axios.post(`https://request-forwarder.cognigy.ai/forward`, body, {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'X-API-Key': args.secret.api_key
+                'X-API-Key': api_key
             }
-        }).then((response) => {
-            input.context.getFullContext()[args.store] = response.data;
-            resolve(input);
-        }).catch((err) => {
-            if (args.stopOnError) { reject(err.message); return; }
-            input.context.getFullContext()[args.store] = { "error": err.message };
-            resolve(input);
         });
-    });
+
+        input.actions.addToContext(contextStore, response.data, 'simple');
+    } catch (error) {
+        if (stopOnError) {
+            throw new Error(error.message);
+        } else {
+            input.actions.addToContext(contextStore, { error: error.message }, 'simple');
+        }
+    }
+
+    return input;
 }
 
 module.exports.RunRobot = RunRobot;
