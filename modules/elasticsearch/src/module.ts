67861563235f996997324fdd9001ae7f01a4fe7f
Alexander Teusz <a.teusz@cognigy.com>
2019-06-21 16:40:35 +0200

    basic node for search

--- modules/elasticsearch/src/module.ts
+++ modules/elasticsearch/src/module.ts
@@ -1,43 +1,41 @@
 import elasticsearch from 'elasticsearch';
 
 /**
- * Finds spelling mistakes and predicts the correct word.
+ * Simple elastic search.
  * @arg {SecretSelect} `secret` The configured secret to use
- * @arg {CognigyScript} `text` The text to check
- * @arg {Select[ar,zh-CN,zh-HK,zh-TW,da,nl-BE,nl-NL,en-AU,en-CA,en-IN,en-ID,en-MY,en-NZ,en-PH,en-ZA,en-GB,en-US,fi,fr-BE,fr-CA,fr-FR,fr-CH,de-AT,de-DE,de-CH,it,ja,ko,no,pl,pt-BR,pt-PT,ru,es-AR,es-CL,es-MX,es-ES,es-US,sv,tr]} `language` The texts language
- * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
- * @arg {CognigyScript} `store` Where to store the result
+ * @arg {CognigyScript} `query` The search query
+ * @arg {CognigyScript} `contextStore` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function simpleSearch(input: IFlowInput, args: { secret: CognigySecret, text: string, language: string, writeToContext: boolean, store: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
-    // Check if secret exists and contains correct parameters
-    if (!args.secret || !args.secret.key || !args.secret.host) return Promise.reject("Secret not defined or invalid.");
-    if (!args.text) return Promise.reject("No text defined.");
+async function simpleSearch(input: IFlowInput, args: { secret: CognigySecret, query: string, language: string, contextStore: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
 
-    return new Promise((resolve, reject) => {
-        let result = {};
+    const { secret, query, contextStore, stopOnError } = args;
+    const { host } = secret;
 
-        const client = new elasticsearch.Client({
-            host: args.secret.host,
-            log: 'trace'
-        });
+    if (!secret) throw new Error('No secret defined');
+    if (!host) throw new Error("The secret is missing the 'host' field");
+    if (!query) throw new Error("No search query defined");
+    if (!contextStore) throw new Error("No context store defined");
 
-        client.search({
-            q: 'pants'
-        }).then( (body) => {
-            result = body.hits.hits;
-            if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-            else input.input[args.store] = result;
-            resolve(input);
-        }, (err) => {
-            if (args.stopOnError) { reject(err.message); return; }
-            result = { "error": err.message };
-            if (args.writeToContext) input.context.getFullContext()[args.store] = result;
-            else input.input[args.store] = result;
-            resolve(input);
-        });
 
+    const client = new elasticsearch.Client({
+        host,
+        log: 'trace'
     });
+
+    try {
+        const body = await client.search({q: query});
+
+        input.actions.addToContext(contextStore, body, 'simple');
+    } catch (error) {
+        if (stopOnError) {
+            throw new Error(error.message);
+        }
+        input.actions.addToContext(contextStore, { error: error.message }, 'simple');
+    }
+
+    return input;
 }
+
 module.exports.simpleSearch = simpleSearch;
 
