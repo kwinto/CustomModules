cc354815739b582d37e6bcfdcb2b0fe7af4c918d
Alexander Teusz <a.teusz@cognigy.com>
2019-06-24 09:53:04 +0200

    new node name

--- modules/elasticsearch/src/module.ts
+++ modules/elasticsearch/src/module.ts
@@ -7,7 +7,7 @@ import * as elasticsearch from 'elasticsearch';
  * @arg {CognigyScript} `contextStore` Where to store the result
  * @arg {Boolean} `stopOnError` Whether to stop on error or continue
  */
-async function simpleSearch(input: IFlowInput, args: { secret: CognigySecret, query: string, contextStore: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
+async function searchSimple(input: IFlowInput, args: { secret: CognigySecret, query: string, contextStore: string, stopOnError: boolean }): Promise<IFlowInput | {}>  {
 
     const { secret, query, contextStore, stopOnError } = args;
     const { host } = secret;
@@ -37,7 +37,7 @@ async function simpleSearch(input: IFlowInput, args: { secret: CognigySecret, qu
     return input;
 }
 
-module.exports.simpleSearch = simpleSearch;
+module.exports.searchSimple = searchSimple;
 
 
 /**
