bd9abef49def729f31dd3ad75998ddd6447cd7e2
Alexander Teusz <a.teusz@cognigy.com>
2019-09-10 11:54:39 +0200

    initial commit

---
+++ modules/sharepoint/README.md
@@ -0,0 +1,16 @@
+# Custom Module: Microsoft Sharepoint
+
+**Secret:**
+
+- key: username
+    - value: Microsoft Login username (e.g. email)
+- key: password
+    - value: Microsoft Login password
+
+## Node: Get Sharepoint Site Info
+
+Returns the entire information of a given Sharepoint site.
+
+## Node: Get Sharepoint List Items
+
+This node returns all items of a specified list.
---
+++ modules/sharepoint/src/module.js
@@ -0,0 +1,107 @@
+const spauth = require('node-sp-auth');
+const request = require('request-promise');
+
+/**
+ * Get entire information of sharepoint site
+ * @arg {SecretSelect} `secret` The configured secret to use
+ * @arg {CognigyScript} `url` The API request url
+ * @arg {CognigyScript} `contextStore` Where to store the result
+ * @arg {Boolean} `stopOnError` Whether to stop on error or continue
+ */
+async function getSharepointSiteInfo(input, args) {
+
+  /* validate node arguments */
+  const { secret, url, contextStore, stopOnError } = args;
+  if (!secret) throw new Error("Secret not defined.");
+  if (!url) throw new Error("The request url is not defined.");
+  if (!contextStore) throw new Error("Context store not defined.");
+  if (stopOnError === undefined) throw new Error("Stop on error flag not defined.");
+
+  /* validate secrets */
+  const { username, password } = secret;
+  if (!username) throw new Error("Secret is missing the 'username' field.");
+  if (!password) throw new Error("Secret is missing the 'password' field.");
+
+  try {
+
+    const data = await spauth.getAuth(url, {
+      username,
+      password
+    });
+
+    let headers = data.headers;
+    headers['Accept'] = 'application/json;odata=verbose';
+
+    const response = await request.get({
+      url: `${url}/_api/web`,
+      headers: headers,
+      json: true
+    });
+
+    input.actions.addToContext(contextStore, response, 'simple');
+  } catch (error) {
+    if (stopOnError) {
+      throw new Error(error.message);
+    } else {
+      input.actions.addToContext(contextStore, { error: error.message }, 'simple');
+    }
+  }
+
+  return input;
+}
+
+module.exports.getSharepointSiteInfo = getSharepointSiteInfo;
+
+
+/**
+ * Get entire information of sharepoint site
+ * @arg {SecretSelect} `secret` The configured secret to use
+ * @arg {CognigyScript} `url` The API request url
+ * @arg {CognigyScript} `list` The sharepoint list
+ * @arg {CognigyScript} `contextStore` Where to store the result
+ * @arg {Boolean} `stopOnError` Whether to stop on error or continue
+ */
+async function getSharepointListItems(input, args) {
+
+  /* validate node arguments */
+  const { secret, url, list, contextStore, stopOnError } = args;
+  if (!secret) throw new Error("Secret not defined.");
+  if (!url) throw new Error("The request url is not defined.");
+  if (!list) throw new Error("The sharepoint list is not defined.");
+  if (!contextStore) throw new Error("Context store not defined.");
+  if (stopOnError === undefined) throw new Error("Stop on error flag not defined.");
+
+  /* validate secrets */
+  const { username, password } = secret;
+  if (!username) throw new Error("Secret is missing the 'username' field.");
+  if (!password) throw new Error("Secret is missing the 'password' field.");
+
+  try {
+
+    const data = await spauth.getAuth(url, {
+      username,
+      password
+    });
+
+    let headers = data.headers;
+    headers['Accept'] = 'application/json;odata=verbose';
+
+    const response = await request.get({
+      url: `${url}/_api/lists/getbytitle('${list}')/items`,
+      headers: headers,
+      json: true
+    });
+
+    input.actions.addToContext(contextStore, response, 'simple');
+  } catch (error) {
+    if (stopOnError) {
+      throw new Error(error.message);
+    } else {
+      input.actions.addToContext(contextStore, { error: error.message }, 'simple');
+    }
+  }
+
+  return input;
+}
+
+module.exports.getSharepointListItems = getSharepointListItems;
