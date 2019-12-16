8a832b9774882f424ee1cef50df3ea21bf92d2ef
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2019-12-16 08:27:40 +0100

    Clean up

--- modules/zipcode-city/src/module.ts
+++ modules/zipcode-city/src/module.ts
@@ -1,41 +1,37 @@
 /**
- * Reads zipcode_germany out from keyphrases and if any returns City/State for that zipcode
+ * Reads zipcode_germany out from keyphrases and if any returns {city: CITY_NAME, city_addon: CITY_NAME_ADDON, state: STATE_NAME}
  * @arg {Boolean} `writeToContext` Whether to write to Cognigy Context (true) or Input (false)
  * @arg {CognigyScript} `store` Where to store the result
  */
 async function zipcodeToCity(
-	input: IFlowInput,
-	args: {
-		writeToContext: boolean;
-		store: string;
-		stopOnNothingFound: boolean;
-	},
-): Promise<IFlowInput | {}> {
-	return new Promise((resolve, reject) => {
+  input: IFlowInput,
+  args: {
+    writeToContext: boolean;
+    store: string;
+  }
+): Promise<IFlowInput> {
+  return new Promise(resolve => {
 		let result = {};
-		const data = require("./data.de.json");
-
-		if (input.input?.keyphrases?.zipcode_germany) {
-			const zipcode = Number(input.input.keyphrases["zipcode_germany"][0].keyphrase);
-			const city = data.find(city => city.Plz === zipcode);
-
+    if (input.input?.keyphrases?.zipcode_germany) {
+      const data = require("./data.de.json");
+      const zipcode = input.input.keyphrases.zipcode_germany[0].keyphrase;
+      const city = data.find(city => String(city.Plz) === String(zipcode));
 			if (city) {
 				result = {
 					city: city.Ort,
 					city_addon: city.Zusatz,
-					state: city.Bundesland,
+					state: city.Bundesland
 				};
+				if (args.writeToContext) {
+					input.context.setContext(args.store, result);
+				} else {
+					input.input[args.store] = result;
+				}
 			}
-		}
+    }
 
-		if (args.writeToContext) {
-			input.context.setContext(args.store, result);
-		} else {
-			input.input[args.store] = result;
-		}
-
-		resolve(input);
-	});
+    resolve(input);
+  });
 }
 
 module.exports.zipcodeToCity = zipcodeToCity;
